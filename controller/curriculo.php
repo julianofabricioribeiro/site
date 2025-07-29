67b2dde92f2c5889dd8a5dd25568b23e4d9bde90
julianofabricioribeiro <juliano.ribeiro.ramos@escola.pr.gov.br>
2025-07-29 09:28:21 -0300

    Update curriculo.php

--- controller/curriculo.php
+++ controller/curriculo.php
@@ -1,1 +1,48 @@
+<?php
+$nome = $_POST["nome"];
+$telefone = $_POST["telefone"];
+$email = $_POST["email"];
+$endereco = $_POST["endereco"];
+
+
+$efc = $_POST["efc"];
+$emc = $_POST["emc"];
+$esc = $_POST["esc"];
+$pgc = $_POST["pgc"];
+$mc = $_POST["mc"];
+
+$mc = $_POST["empresa"];
+$mc = $_POST["data_admissao"];
+$mc = $_POST["data_demissao"];
+$mc = $_POST["atividade_exercida"];
+
+$descricao = $_POST["descricao"];
+$horas = $_POST["horas"];
+$ano_conclusao = $_POST["conclusao"];
+
+$informacoes = $_POST["informacoes"];
+
+echo "DADOS PESSOAIS"
+echo $nome;
+echo $telefone;
+echo $email;
+echo $endereco;
+echo "ESCOLARIDADE";
+echo $efc;
+echo $emc;
+echo $esc;
+echo $pgc;
+echo $mc;
+echo "EXPERIENCIA PROFISSIONAL"
+echo $data_emissao;
+echo $atividade_exercidada;
+echo "CURSOS E IDIOMAS" ;
+echo $descricao;
+echo $horas;
+echo $ano_conclusao;
+echo "INFORMAÇÕES ADICIONAIS";
+echo $informacoes;
+ 
+
+?>
 
